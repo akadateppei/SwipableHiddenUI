6ae1c125b3fb9ad1abe3a0db583b8b2df7aae92a
Teppei Akada <teppei.akada@sidefeed.com>
2021-10-20 20:34:42 +0900

    add Demo

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by AKADA TEPPEI on 2021/10/20.
+//
+
+import SwiftUI
+import SwipableHiddenUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by AKADA TEPPEI on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
